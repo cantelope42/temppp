4a27d7499ac75eb551cd68ebdb1149250567b61a
cantelope <135386830+cantelope42@users.noreply.github.com>
2026-04-17 14:52:12 -0700

    Add files via upload

--- coordinates.js
+++ coordinates.js
@@ -3358,9 +3358,9 @@ const InitPartitioning = geometry => {
     ax /= ct
     ay /= ct
     az /= ct
-    var px = (ax / g.partitionSize + .5) | 0
-    var py = (ay / g.partitionSize + .5) | 0
-    var pz = (az / g.partitionSize + .5) | 0
+    var px = ((ax - minX) / g.partitionSize) | 0
+    var py = ((ay - minY) / g.partitionSize) | 0
+    var pz = ((az - minZ) / g.partitionSize) | 0
     var part = px + py * ctX + pz * ctX * ctY
     for(var m = 0; m<9; m++){
       g.partitions.parts[part].vertices.push(g.vertices[i+m])
