43b63473127536f943afcb4552d4fd4e50f16ec9
cantelope <135386830+cantelope42@users.noreply.github.com>
2026-04-22 15:11:29 -0700

    Add files via upload

--- coordinates.js
+++ coordinates.js
@@ -6332,7 +6332,7 @@ const ProcessShapeArray = shape => {
       tz = data[shpIdx].oz
       var roll, pitch, yaw, rotationMode
       if(shape.shapeArrayIsSprite){
-        pitch = -shape.renderer.pitch - shape.pitch
+        pitch = (-shape.renderer.pitch - shape.pitch) * (renderer.camMode == 'fps' ? 1 : -1)
         yaw   = -shape.renderer.yaw - shape.yaw
         roll  = shape.renderer.roll - shape.roll
         rotationMode = 1
